65827692a072aaa3fa0e32756de2913fd070cd5c
Tom Gibson <tom.gibson@outlook.com>
2019-07-20 19:46:33 +0100

    Add initial status page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using Relay.Models;
 
 namespace Relay.Pages
 {
-    public class IndexModel : PageModel
+    public class Index : PageModel
     {
         private readonly LineupContext _context;
         
-        public IndexModel(LineupContext context)
-        {
-            _context = context;
-        }
-        
-        public void OnGet()
-        {
+        public RelayConfiguration Config { get; }
 
+        public IEnumerable<LineupEntry> Channels =>
+            _context.Entries.OrderBy(e => e.Number);
+
+        public int HdCount =>
+            _context.Entries.Count(e => e.HD == 1);
+        
+        public int FavouritesCount =>
+            _context.Entries.Count(e => e.Favorite == 1);
+
+        public int ChannelCount => _context.Entries.Count();
+
+        public Index(
+            IOptionsSnapshot<RelayConfiguration> config,
+            LineupContext context)
+        {
+            Config = config.Value;
+            _context = context;
         }
     }
 }
