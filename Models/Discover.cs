0a46a527d2f8f22aed7006ff2a5c998bbbd32116
Tom Gibson <tom.gibson@outlook.com>
2019-09-22 03:08:56 +0000

    Fixing case for json values in discovery

--- Models/Discover.cs
+++ Models/Discover.cs
@@ -4,20 +4,28 @@ namespace Relay.Models
 {
     public class Discover
     {
+        [JsonProperty("FriendlyName")]
         public string FriendlyName { get; set; }
 
+        [JsonProperty("Manufacturer")]
         public string Manufacturer { get; set; } = "Silicondust";
         
+        [JsonProperty("ModelNumber")]
         public string ModelNumber { get; set; }
 
+        [JsonProperty("FirmwareName")]
         public string FirmwareName { get; } = "hdhomerun_atsc";
         
+        [JsonProperty("TunerCount")]
         public int TunerCount { get; set; }
 
+        [JsonProperty("FirmwareVersion")]
         public string FirmwareVersion { get; } = "1337";
 
+        [JsonProperty("DeviceID")]
         public string DeviceId { get; set; }
 
+        [JsonProperty("DeviceAuth")]
         public string DeviceAuth { get; } = "NoAuth";
         
         [JsonProperty("BaseURL")]
